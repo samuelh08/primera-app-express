d2a1b8164bf7878ccdf0018353e0b15c4e8cd381
Samuel <samuelh08.shm@gmail.com>
2022-04-08 17:20:24 -0500

    adds query reading

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo</h1>');
+  if (req.query.nombre === undefined) {
+    res.send('<h1>Hola desconocido!</h1>');
+  } else {
+    res.send('<h1>Hola ' + req.query.nombre + '!</h1>');
+  }
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
