d6d181decf80c8f073026d48401fff4ec49d8d44
Samuel <samuelh08.shm@gmail.com>
2022-05-11 20:33:27 -0500

    shows the browser using the app

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded());
-
 app.get('/', (req, res) => {
-  res.send(`<form action="/hello" method="post">
-  <label for="name"><input type="text" id="name" name="name">
-  <button type="submit">Enviar</button>
-</form>`);
-});
-
-app.post('/hello', (req, res) => {
-  res.send('<h1>Hola ' + req.body.name + '!</h1>');
+  const browser = req.get('sec-ch-ua');
+  res.send(browser);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
